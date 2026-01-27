1116867dc756db08136721768d46db9b0a15aa99
Chrysanthos Pepes <pepesxrisanthos@gmail.com>
2026-01-27 12:46:50 +0200

    chore: add system prompt on agent

--- app/agent/calendar_agent.py
+++ app/agent/calendar_agent.py
@@ -6,12 +6,18 @@ from app.tools.list_events import list_next_events_tool, list_today_events_tool
 
 from app.config.settings import load_settings
 
+from app.agent.prompts import SYSTEM_PROMPT
+
 class CalendarAgent:
     def __init__(self):
         self._settings = load_settings()
         self.llm = ChatOpenAI(model=self._settings.openai_model, temperature=0, api_key=self._settings.openai_api)
         self.tools = [create_event_tool, list_next_events_tool, list_today_events_tool]
-        self.agent = create_agent(self.llm, self.tools)
+        self.agent = create_agent(
+            model=self.llm,
+            tools=self.tools,
+            system_prompt=SYSTEM_PROMPT
+            )
         
     def run(self, user_prompt: str) -> str:
         result = self.agent.invoke({"messages": [("user", user_prompt)]})
